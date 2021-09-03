3bfa14a86b2114cb42ab76f807dae7229a56cbd0
Your name <Magorzata abendowicz>
2021-09-03 10:34:57 +0200

    new variable

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,9 @@
 public class Main {
     public static void main (String args[]){
-        System.out.println("Hello World");
-        System.out.println("Hello everyone!");
-        System.out.println("Hello" + " others!");
-        System.out.println("Hiiiia\tcossss");
-        System.out.println("Hiiiia\n");
-        System.out.println("Hiiiia");
-        System.out.println("Hiiiia \"cossss\"");
-        //komentarz
-        /**
+        String text;
+        text = "Dajemy jakiś tekst";
 
-        Car Opel = new Car();
-        Car Mazda = new Car("Fajna", "czerwona", 1999, "petrol");
-        Mazda.startEngine();
-        Mazda.startEngine2();
-        Opel.stopEngine(20);
-         **/
+        System.out.println(text);
 
     }
 }
