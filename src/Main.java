812db30d309e6f0c15be7492998309d7c3cd8667
Your name <Magorzata abendowicz>
2021-09-03 10:43:28 +0200

    character testing

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main (String args[]){
         String text = "Dajemy jakiś tekst";
+        char Nazwa1 = 'p';
+        char Nazwa2 = '2';
 
         System.out.println(text);
+        Character.isDigit(Nazwa1);
+        System.out.println(Character.isDigit(Nazwa1));
+        System.out.println(Character.isDigit(Nazwa2));
 
     }
 }
