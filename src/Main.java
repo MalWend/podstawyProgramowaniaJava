f803436a8096c46109ce0aa666600c3d543782c0
Your name <Magorzata abendowicz>
2021-09-07 12:37:55 +0200

    add to ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main{
     public static void main(String[] args) {
 
@@ -5,6 +7,16 @@ public class Main{
         Opel.newMethod();
         Car Skoda = new Car("super", "zielony", 2004, "petrol");
         Skoda.ifIBuy();
+        Car Toyota = new Car("Razor", "blue", 2021, "diesel");
+        Car Fiat = new Car ("Uno FIRE","silver",2000,"petrol");
+        Car Mercedes = new Car ("C 300", "yellow", 1986, "petrol");
+
+        ArrayList<Car> carTable = new ArrayList<>();
+        carTable.add(Mercedes);
+        carTable.add(Opel);
+        carTable.add(Skoda);
+        carTable.add(Fiat);
+        carTable.add(Toyota);
     }
 
     }
